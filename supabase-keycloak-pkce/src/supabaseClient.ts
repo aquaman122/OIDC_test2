257792c8eaabbf9d6cf0f866210c5a9fead18e01
aquaman122 <swj981128@gmail.com>
2025-06-06 17:18:23 +0900

    fix: 정보 env파일로 이동

--- supabase-keycloak-pkce/src/supabaseClient.ts
+++ supabase-keycloak-pkce/src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dofbnufqodacemdmwxqy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvZmJudWZxb2RhY2VtZG13eHF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NzYwNzcsImV4cCI6MjA1ODU1MjA3N30.8N6S1DhDgVvglD-hh9ijbbre4CZzXdVFbw6ak1kn1j4';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
